064d29e0e8af276d240a992daf86c45d45bfddc0
Jason Devaraj <jdevaraj01@gmail.com>
2025-11-22 21:20:00 -0500

    ToDo 2.

--- app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
@@ -1,7 +1,9 @@
 package edu.farmingdale.threadsexample
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -12,11 +14,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.NumberFormat
 import java.util.Locale
+import kotlin.text.format
 
 @Composable
 fun FibonacciDemoNoBgThrd() {
@@ -49,4 +58,54 @@ fun FibonacciDemoNoBgThrd() {
 
 fun fibonacci(n: Long): Long {
     return if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+@Composable
+fun FibonacciDemoWithCoroutine() {
+    var answer by remember { mutableStateOf("") }
+    var textInput by remember { mutableStateOf("10") }
+    var fibonacciJob: Job? by remember { mutableStateOf(null) }
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )        {
+
+
+            TextField(
+                value = textInput,
+                onValueChange = { textInput = it },
+                label = { Text("Enter a number") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+
+            Button(onClick = {
+                val num = textInput.toLongOrNull() ?: 0L
+                fibonacciJob = coroutineScope.launch {
+                    val fibNumber = withContext(Dispatchers.Default) {
+                        fibonacciSuspend(num)
+                    }
+                    answer = NumberFormat.getNumberInstance(Locale.US).format(fibNumber)
+                }
+            }) {
+                Text("Start")
+            }
+
+            Button(onClick = { fibonacciJob?.cancel() }) {
+                Text("Cancel")
+            }
+
+
+        Text("Result: $answer")
+    }
+}
+
+suspend fun fibonacciSuspend(n: Long): Long {
+    //delay(10)
+    return if (n <= 1) n else fibonacciSuspend(n - 1) +
+            fibonacciSuspend(n - 2)
 }
--- app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
@@ -34,11 +34,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 
-    FibonacciDemoNoBgThrd()
+    //FibonacciDemoNoBgThrd()
 
+    FibonacciDemoWithCoroutine()
 }
 
-// ToDo 1: Call `FibonacciDemoNoBgThrd` that calculates the Fibonacci number of a given number.
+
+
 // ToDo 2: Create a composable function called `FibonacciDemoWithCoroutine` that calculates the
 //  Fibonacci number of a given number using a coroutine.
 // ToDo 3: Start the application using the CountDownActivity
